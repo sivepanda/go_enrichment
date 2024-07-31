8fe0a950e7d6f60bef10a0782dd394b890d36f50
sivepanda <sivenpanda@icloud.com>
2024-07-31 03:55:08 -0500

    Working version

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # go_enrichment
+## Introduction
+This project aims to create p-values for the abundance of each GO Categories in each chromosome queried by doing a T-Test of number of hits in a particular chromosome vs those that are NOT on the chromosome. This helps us identify if there is a significant correlation between a particular GO Category and a chromosome.
+
+# Usage
+Usage is fairly simple, as the program is designed to be a python library. To run, all you need to do is install the library with `pip install .` in the main directory in the project. This will install dependencies as well. Then, you can run `python -m go_enrichment.enrichment {output file} {number of hits}`. Be sure to replace the output file and number of hits parameters with your desired values.
--- go_enrichment/enrichment.py
+++ go_enrichment/enrichment.py
@@ -4,21 +4,26 @@ from scipy.stats import fisher_exact
 from go_enrichment.utils import generate_contingency_table
 from tqdm import tqdm
 
-def enrichment_analysis(go_data):
-    max = 10 
-    go_progress_bar = tqdm(total=max, desc=f'Getting GO Data')
+
+
+# This program uses the mygene.info API, an example you can visit to see usual output of this is below
+# https://mygene.info/v3/query?q=go:*&fields=go,genomic_pos_hg19.chr&fetch_all=TRUE
+
+def enrichment_analysis(go_data, num_points):
+    go_progress_bar = tqdm(total=num_points, desc=f'Getting GO Data')
     
     results = []
     go_dat = []
-    go_categories = set()
+    go_ids = set()
     chromosomes = set()
+    go_info = {} # Dictionary that has the GO ID as the Key and the Term and Category within it
     n = 0
     
     # Collect all unique GO categories and chromosomes from input data stream
     for hit in go_data:
         go_progress_bar.update(1)
         n += 1
-        if n == max: 
+        if n == num_points: 
             go_data.close()
             break
         
@@ -30,25 +35,29 @@ def enrichment_analysis(go_data):
             else:
                 chromosomes.add(hit['genomic_pos_hg19']['chr'])
 
-
+        # So that both the BP and MP are queried for each response
         for aspect in hit['go']:
             if isinstance(hit['go'][aspect], list):
                 for item in hit['go'][aspect]:
-                    go_categories.add(item['id'])
+                    go_ids.add(item['id'])
+                    go_info[item['id']] = { 'category': aspect , 'term': item['term'] }
             else:
-                go_categories.add(hit['go'][aspect]['id'])
+                go_ids.add(hit['go'][aspect]['id'])
+                go_info[hit['go'][aspect]['id']] = { 'category': aspect, 'term':  hit['go'][aspect]['term'] }
+
 
 
     print("Generating Contingency tables")
-    print(len(go_categories))
-    for category in go_categories:
-        progress_bar = tqdm(total=len(chromosomes), desc=f'Generating Contingency Tables for GO Category {category}')
+    for id in go_ids:
+        progress_bar = tqdm(total=len(chromosomes), desc=f'Generating Contingency Tables for GO Category')
         for chr in chromosomes:
-            table = generate_contingency_table(go_dat, category, chr)
+            table = generate_contingency_table(go_dat, id , chr)
             progress_bar.update(1)
             oddsratio, p_value = fisher_exact(table)
             results.append({
-                'go_category': category,
+                'go_id': id,
+                'go_category': go_info[id]['category'],
+                'go_name': go_info[id]['term'],
                 'chromosome': chr,
                 'odds_ratio': oddsratio,
                 'p_value': p_value
@@ -61,11 +70,12 @@ def main():
 
     parser = argparse.ArgumentParser(description='Perform GO enrichment analysis on chromosome data.')
     parser.add_argument('output_file', help='Path to save the enrichment analysis results CSV file.')
+    parser.add_argument('num_points', help='The number of GO IDs you would like to query.')
 
     args = parser.parse_args()
 
     go_data = get_go_data()
-    results = enrichment_analysis(go_data)
+    results = enrichment_analysis(go_data, int(args.num_points))
     results.to_csv(args.output_file, index=False)
 
 if __name__ == "__main__":
