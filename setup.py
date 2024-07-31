a160fd34824a4e7367510f8e24f51c79b47e882e
sivepanda <siven-panda@omrf.org>
2024-07-31 12:41:19 -0500

    Make command work

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
+print(find_packages())
 
 setup(
     name='go_enrichment',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(include=['go_enrichment']),
     install_requires=[
+        'numpy<2',
+        'tqdm',
         'pandas',
+        'mygene',
         'scipy',
-        # Add other dependencies as needed
     ],
     entry_points={
         'console_scripts': [
