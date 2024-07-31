673318aece27458517d9e6bf1b3b15ea3a9f275a
sivepanda <siven-panda@omrf.org>
2024-07-31 16:38:59 -0500

    Fixed some oopsies

--- go_enrichment/enrichment.py
+++ go_enrichment/enrichment.py
@@ -3,6 +3,7 @@ from go_enrichment.get_go import get_go_data
 from scipy.stats import fisher_exact
 from go_enrichment.utils import generate_contingency_table
 from tqdm import tqdm
+import concurrent.futures
 
 
 
@@ -12,7 +13,6 @@ from tqdm import tqdm
 def process_chromosome(go_dat, go_info, id, chr):
     # for chr in chromosomes:
     table = generate_contingency_table(go_dat, id , chr)
-    progress_bar.update(1)
     oddsratio, p_value = fisher_exact(table)
     result = ({
         'chromosome': chr,
@@ -67,17 +67,15 @@ def enrichment_analysis(go_data, num_points):
 
 
     print("Generating Contingency tables")
-    for id in go_ids:
-        progress_bar = tqdm(total=len(chromosomes), desc=f'Generating Contingency Tables for GO Category')
-         with concurrent.futures.ThreadPoolExecutor() as executor:
-                for id in go_ids:
-                    progress_bar = tqdm(total=len(chromosomes), desc=f'Generating Contingency Tables for GO Category {id}')
-                    # Create a list of futures
-                    futures = {executor.submit(process_chromosome, go_dat, go_info, id, chr): chr for chr in chromosomes}
-                    for future in concurrent.futures.as_completed(futures):
-                        result = future.result()
-                        results.append(result)
-                        progress_bar.update(1)   
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+       for id in go_ids:
+           progress_bar = tqdm(total=len(chromosomes), desc=f'Generating Contingency Tables for GO Category {id}')
+           # Create a list of futures
+           futures = {executor.submit(process_chromosome, go_dat, go_info, id, chr): chr for chr in chromosomes}
+           for future in concurrent.futures.as_completed(futures):
+               result = future.result()
+               results.append(result)
+               progress_bar.update(1)   
     
     return pd.DataFrame(results)
 
